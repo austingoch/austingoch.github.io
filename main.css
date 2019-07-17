5e738e589d69812b41fceec2a17c4a233afa13b7
Austin Goch <49793780+austingoch@users.noreply.github.com>
2019-07-17 16:41:46 -0400

    Add files via upload

---
+++ main.css
@@ -0,0 +1,46 @@
+body 
+{
+    background-color:   #fefbd8;
+}
+#wrapper
+{
+    margin: 0 auto;
+    width:  80%;
+}
+nav ul, footer ul 
+{
+    font-family:'Helvetica', 'Arial', 'Sans-Serif';
+    padding: 0;
+    list-style: none;
+    font-weight: bold;
+}
+nav ul li, footer ul li 
+{
+    display: inline;
+    margin-right: 20px;
+}
+a 
+{
+    text-decoration: none;
+    color: #999;
+}
+a:hover 
+{
+    text-decoration: underline;
+}
+h1 
+{
+    font-size: 3em;
+    font-family:'Helvetica', 'Arial', 'Sans-Serif';
+}
+p 
+{
+    font-size: 1.5em;
+    line-height: 1.4em;
+    color: #333;
+}
+footer 
+{
+    border-top: 1px solid #d5d5d5;
+    font-size: .8em;
+}
